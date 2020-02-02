36fe6a4c870627a098e30c8a6d42e73bb107f479
samuelchou <samuelchou@hotmail.com.tw>
2020-02-02 14:59:59 +0800

    新增：標題畫面、背景畫面、背景音樂。調整了選單與關卡內容。

---
+++ Assets/Scripts/UI/Rotate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private RectTransform rectTransform;
+
+    public float rotateSpeed = 30f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rectTransform != null)
+        {
+            Quaternion q = rectTransform.rotation;
+            q.z += rotateSpeed;
+            rectTransform.Rotate(Vector3.forward, rotateSpeed);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/UI/UIRandomShaking.cs
@@ -0,0 +1,35 @@
+﻿//UI Random Shaking made by STC
+//Contact:          stc.ntu@gmail.com
+//Last maintained:  2017/09/15
+//Usage:            Make something on UI shake. CAUTION: Might be a little annoying.
+//Notice:           It require a RectTransform to use it. If the thing don't get one, the script will summon one.
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class UIRandomShaking : MonoBehaviour
+{
+
+    
+    public float[] leftUpPoint = new float[2] { -5, 5 };
+    public float[] rightDownPoint = new float[2] { 5, -5 };
+
+    private RectTransform rectTransform;
+    private Vector2 newPosition;
+    
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        //Debug.Log(rectTransform.anchoredPosition);
+    }
+    
+    void Update()
+    {
+        newPosition = new Vector2(
+            Random.Range(leftUpPoint[0], rightDownPoint[0]),
+            Random.Range(leftUpPoint[1], rightDownPoint[1]));
+        rectTransform.anchoredPosition = newPosition;
+
+    }
+}
