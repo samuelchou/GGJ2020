e1ba364749086726877325406d9dc3da4baa20e4
samuelchou <samuelchou@hotmail.com.tw>
2020-02-02 11:18:56 +0800

    修正 - 尖刺：現在可以自定義損失血量了。

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
+    public int damage = 20; // 作用於玩家身上
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            //PlayerControl.instance.GetDamage(20);
+            PlayerControl.instance.GetDamage(damage);
             if (collision.gameObject.transform.position.x > transform.position.x)
                 PlayerControl.instance.GiveForce(new Vector2(250, 300));
             else
