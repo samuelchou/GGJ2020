750703e24109ea9a35aee5a929c6da86d4ded5ca
samuelchou <samuelchou@hotmail.com.tw>
2020-02-02 17:00:54 +0800

    最後衝刺！開場。

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public float waitTime = 30f;
+
+    private float timePassed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > waitTime)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PathMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMove : MonoBehaviour
+{
+    public Transform endPoint = null;
+    public float velocity = 30f;
+    public float waitTime = 3f;
+
+    private Vector3 path = Vector3.zero;
+    private Vector3 moveSpeed;
+    private float timeNeed, timePassed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (endPoint != null)
+        {
+            path = endPoint.position - transform.position;
+            moveSpeed = path.normalized * velocity;
+            timeNeed = path.magnitude / velocity;
+        }
+    }
+
+    private bool reachEnd = false;
+    private float distAccepted = 0.05f;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timePassed > waitTime && !reachEnd)
+        {
+            if (Vector3.Distance(transform.position, endPoint.position) <= distAccepted || timePassed >= timeNeed + waitTime)
+            {
+                reachEnd = true;
+                transform.position = endPoint.position;
+            }
+            if (!reachEnd)
+            {
+                transform.position += moveSpeed * Time.deltaTime;
+            }
+        }
+        timePassed += Time.deltaTime;
+
+    }
+}
